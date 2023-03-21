2a7669b9d4ecaad917c3e99b88912ec66e5d38dd
yermushy31 <124054670+yermushy31@users.noreply.github.com>
2023-03-21 14:36:48 +0100

    Update config.php

--- SiteFAQ/config.php
+++ SiteFAQ/config.php
@@ -5,8 +5,4 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'site');
 define('DB_PORT', '3306');
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
 ?>
