dcd5e6acbdf4b4ffb5bbfc758575d623d4d6dbda
LoutrePasSauvage <123477561+LoutrePasSauvage@users.noreply.github.com>
2023-03-28 15:31:25 +0200

    indentation

--- SiteFAQ/config.php
+++ SiteFAQ/config.php
@@ -1,8 +1,7 @@
 <?php 
- 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'site');
-define('DB_PORT', '3306');
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'site');   /*Selon le nom de la base de donnée, modifier "site" par le nom de votre BDD*/
+    define('DB_PORT', '3306');
 ?>
