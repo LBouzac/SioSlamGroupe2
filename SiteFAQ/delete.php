7176963b63d7e3fda1f16aef2a0cc3a47ecfb73e
LoutrePasSauvage <123477561+LoutrePasSauvage@users.noreply.github.com>
2023-03-28 15:35:16 +0200

    ajout du session start

--- SiteFAQ/delete.php
+++ SiteFAQ/delete.php
@@ -1,19 +1,16 @@
 <?php
-include 'config.php';
-try
- 
-{
-    $db = new PDO('mysql:host='.DB_SERVER.';port='.DB_PORT.';dbname='.DB_NAME.'', DB_USERNAME, DB_PASSWORD);
-}
- 
-catch(Exception $e)
- 
-{
+    session_start();
+    include 'config.php';
+    try
+    {
+        $db = new PDO('mysql:host='.DB_SERVER.';port='.DB_PORT.';dbname='.DB_NAME.';charset=utf8mb4', DB_USERNAME, DB_PASSWORD);
+    }
+    catch(Exception $e)
+    {
         die('Erreur : '.$e->getMessage());
-}
-//$id = $_GET['idquestionSupp'];
-$supp = $db->prepare('DELETE FROM faq WHERE idQuestion= ? ');
-$supp->execute([$_GET['idquestionSupp']]);
+    }
+    $supp = $db->prepare('DELETE FROM faq WHERE idQuestion= ? ');
+    $supp->execute([$_GET['idquestionSupp']]);
 
-header('Location: list.php');
- 
+    header('Location: list.php');
+?>
