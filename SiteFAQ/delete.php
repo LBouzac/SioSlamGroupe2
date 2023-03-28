0fe11829269628cb3073e4b5bc339cfc62591d6b
Ludo <123162852+Ludov1234@users.noreply.github.com>
2023-03-28 15:44:51 +0200

    Update delete.php

--- SiteFAQ/delete.php
+++ SiteFAQ/delete.php
@@ -1,16 +1,29 @@
 <?php
     session_start();
     include 'config.php';
-    try
-    {
-        $db = new PDO('mysql:host='.DB_SERVER.';port='.DB_PORT.';dbname='.DB_NAME.';charset=utf8mb4', DB_USERNAME, DB_PASSWORD);
-    }
-    catch(Exception $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
-    $supp = $db->prepare('DELETE FROM faq WHERE idQuestion= ? ');
-    $supp->execute([$_GET['idquestionSupp']]);
 
-    header('Location: list.php');
-?>
+    if ((isset($_POST['submit']))) 
+            {                
+                try
+                {
+                    $db = new PDO('mysql:host='.DB_SERVER.';port='.DB_PORT.';dbname='.DB_NAME.';charset=utf8mb4', DB_USERNAME, DB_PASSWORD);
+                }
+                catch(Exception $e)
+                {
+                    die('Erreur : '.$e->getMessage());
+                }
+                $supp = $db->prepare('DELETE FROM faq WHERE idQuestion= ? ');
+                $supp->execute([$_GET['idquestionSupp']]);
+                
+                header('Location: list.php'); 
+            }
+            else 
+            {
+        ?>
+            <br><br><strong>Etes vous sur de vouloir Supprimer la question ?!</strong>
+            <form method="POST">
+                <p><button type="submit" name="submit" class="boutonacces">Valider</button>
+            </form>
+            <button class="boutonacces"><a href="list.php">Annuler</a></button>
+        <?php
+            }
