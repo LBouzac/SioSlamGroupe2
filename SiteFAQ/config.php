3bd862a5bbc0373ff054b2669822432930d12dc7
LoutrePasSauvage <123477561+LoutrePasSauvage@users.noreply.github.com>
2023-04-22 11:40:53 +0200

    Update config.php

--- SiteFAQ/config.php
+++ SiteFAQ/config.php
@@ -2,6 +2,6 @@
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'site');   /*Selon le nom de la base de donnée, modifier "site" par le nom de votre BDD*/
+    define('DB_NAME', 'sitem2l');   /*Selon le nom de la base de donnée, modifier "site" par le nom de votre BDD*/
     define('DB_PORT', '3306');
 ?>
