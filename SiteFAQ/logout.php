d189e79855aec2630f7a8de75f7a16203336ca69
Ludovic <123162852+Ludov1234@users.noreply.github.com>
2023-04-22 11:18:51 +0200

    icone ajouter

--- SiteFAQ/logout.php
+++ SiteFAQ/logout.php
@@ -13,6 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
+    <link rel="icon" type="img/png" href="img/logo.png">
     <title>logout</title>
 </head>
 
