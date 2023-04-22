e388e9a7f1cc8b3d3cd9845f698abefe42e34187
Ludovic <123162852+Ludov1234@users.noreply.github.com>
2023-04-22 11:21:15 +0200

    icone ajouter

--- SiteFAQ/delete.php
+++ SiteFAQ/delete.php
@@ -29,6 +29,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>answer</title>
     <link rel="stylesheet" href="main.css">
+    <link rel="icon" type="img/png" href="img/logo.png">
 </head>
 
 <body>
